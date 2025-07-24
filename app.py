c7d63a752e7286b1b463546ec80a31bff39ae5f7
Carlos Ramirez <cjyuyo@Mac.lan>
2025-07-24 10:03:29 -0500

    Agrego comentario para forzar despliegue en Render

--- app.py
+++ app.py
@@ -318,3 +318,4 @@ def delete_client(client_id):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+# Forzando la actualización para Render
