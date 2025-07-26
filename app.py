7215fcd9842e21c37f48c85dda0036f9480117c6
Carlos Ramirez <cjyuyo@Mac.lan>
2025-07-26 13:55:20 -0500

    Refactor: Cambia lógica de anulación de inscripción final a reinicio total

--- app.py
+++ app.py
@@ -533,4 +533,4 @@ def delete_client(client_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=Tr
+    app.run(host='0.0.0.0', port=port, debug=True)
